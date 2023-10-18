d10fc3d3a9c6982e5a15bf9c1b5549242ecb7ed1
KshitizAgarwal21 <kshitizagarwal27@gmail.com>
2023-10-18 21:30:17 +0530

    chat ui

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,19 +1,89 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 export default function Chat(props) {
   const { isExpanded, setIsExpanded } = props.data;
+  const [lastconvo, setLastconvo] = useState([]);
+  const [conversation, setConversation] = useState([]);
+  const getChat = async () => {
+    const resp = await axios.get("http://localhost:8080/chat/chatlist", {
+      headers: {
+        Authorization: "652812f0bf41d828c0b78c60",
+      },
+    });
+    const arr = resp.data;
+    // const userids = resp.data.map(async (elem) => {
+
+    //   return await axios.post("http://localhost:8080/auth/getuserDetails", {
+    //     id: friend,
+    //   });
+    // });
+    // Promise.all(userids).then((res) => {
+
+    // });
+
+    const sortedChats = resp.data.map((elem) => {
+      return {
+        _id: elem._id,
+        content: elem.content.sort(
+          (a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt)
+        ),
+      };
+    });
+    console.log(sortedChats);
+    const userData = sortedChats.map(async (elem) => {
+      const lasttexted = elem.content[0].author_id;
+      const getUser = await axios.post(
+        "http://localhost:8080/auth/getuserDetails",
+        { id: lasttexted },
+        {
+          headers: {
+            Authorization: "652812f0bf41d828c0b78c60",
+          },
+        }
+      );
+      return getUser;
+    });
+    let finalArr = [];
+    Promise.all(userData).then((resp) => {
+      console.log(resp);
+      sortedChats.forEach((item, key) =>
+        finalArr.push({
+          _id: item._id,
+          lastMessage: item.content[0].message,
+          author: resp[key].data.first_name,
+        })
+      );
+      setConversation(finalArr);
+    });
+
+    // const finalArrItem = {
+    //   _id:
+
+    // }
+    // const finalArr =
+    // console.log(getUser);
+  };
+
+  useEffect(() => {
+    getChat();
+  }, []);
   return (
     <div>
       <h2>Chat</h2>
       <hr></hr>
-      <Link to="/chat/:id" onClick={() => setIsExpanded(false)}>
-        <div className="recent-chat">Person name: Hello </div>
-      </Link>
 
-      <div className="recent-chat">Person name: Hello </div>
-      <div className="recent-chat">Person name: Hello </div>
-      <div className="recent-chat">Person name: Hello </div>
-      <div className="recent-chat">Person name: Hello </div>
+      {conversation?.map((elem) => {
+        return (
+          <>
+            <Link to={`/chat/${elem._id}`} onClick={() => setIsExpanded(false)}>
+              <div className="recent-chat">
+                {elem.author}: {elem.lastMessage}{" "}
+              </div>
+            </Link>
+          </>
+        );
+      })}
     </div>
   );
 }
--- src/Components/Chat/ChatWindow.js
+++ src/Components/Chat/ChatWindow.js
@@ -1,68 +1,75 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import dp from "../../assets/user.jpeg";
 export default function ChatWindow() {
+  const [conversation, setConversation] = useState([]);
+  const [conversation_details, setConversationDetails] = useState({});
+  const [friend, setFriend] = useState({});
+  const { id } = useParams();
+
+  const getConversation = async () => {
+    const resp = await axios.post(
+      "http://localhost:8080/chat/convo",
+      { conversation_id: id },
+      {
+        headers: {
+          Authorization: "652812f0bf41d828c0b78c60",
+        },
+      }
+    );
+    setConversationDetails(resp.data);
+    setConversation(resp.data.content);
+
+    const friend =
+      resp.data.participants.participant1 == "652812f0bf41d828c0b78c60"
+        ? resp.data.participants.participant2
+        : resp.data.participants.participant1;
+
+    const getUser = await axios.post(
+      "http://localhost:8080/auth/getuserDetails",
+      { id: friend },
+      {
+        headers: {
+          Authorization: "652812f0bf41d828c0b78c60",
+        },
+      }
+    );
+
+    if (getUser.status == 200) {
+      setFriend(getUser.data);
+    }
+  };
+
+  useEffect(() => {
+    getConversation();
+  }, [id]);
+  console.log(conversation);
   return (
     <div>
       <div className="chat-header">
         <img src={dp} /> &nbsp;
-        <p>User Name</p>
+        <p>
+          {friend?.first_name} {friend?.last_name}
+        </p>
       </div>
 
       <div className="chat-body">
-        <div className="incoming">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="outgoing">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="incoming">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="outgoing">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="incoming">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="outgoing">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="incoming">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="outgoing">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="incoming">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="outgoing">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="incoming">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="outgoing">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="incoming">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="outgoing">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="incoming">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="outgoing">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="incoming">
-          <span>hello eqhbwhfsb</span>
-        </div>
-        <div className="outgoing">
-          <span>hello eqhbwhfsb</span>
-        </div>
+        {conversation?.map((elem) => {
+          return (
+            <>
+              {elem.author_id == "652812f0bf41d828c0b78c60" ? (
+                <div className="outgoing">
+                  <span>{elem.message}</span>
+                </div>
+              ) : (
+                <div className="incoming">
+                  <span>{elem.message}</span>
+                </div>
+              )}
+            </>
+          );
+        })}
       </div>
       <div className="chat-actions">
         <input type="text"></input>
